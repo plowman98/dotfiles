10f088845df388773b4a7d11b7d3c3dc352362a2
plowman98 <98.plowman@gmail.com>
2026-03-06 21:42:53 +0900

    Add zsh plugins configuration

---
+++ zsh/.zshrc.d/40-plugins.zsh
@@ -0,0 +1,7 @@
+# 40-plugins.zsh
+
+# zsh-autosuggestions
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh-syntax-highlighting
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
